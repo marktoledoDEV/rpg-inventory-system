3a6431e14b45f73273876740f5e638d31adc5032
Mark Toledo <markalextoledo@gmail.com>
2021-12-21 20:25:55 -0600

    item data added

---
+++ rpg-inventory-system/Assets/Scripts/Item/ItemData.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "rpg-inventory-system/ItemData", order = 2)]
+public class ItemData : ScriptableObject 
+{
+    [SerializeField]
+    private Texture _itemImage;
+
+    [SerializeField]
+    private string _itemName;
+    public string ItemName
+    {
+        get { return _itemName; }
+        set { _itemName = value; }
+    }
+
+    [SerializeField]
+    [TextArea]
+    private string _itemDescription;
+    public string ItemDescription
+    {
+        get { return _itemDescription; }
+        set { _itemDescription = value; }
+    }
+
+    [SerializeField]
+    private int _xSize;
+    public int XSize
+    {
+        get { return _xSize; }
+        set { _xSize = value; }
+    }
+
+    [SerializeField]
+    private int _ySize;
+    public int YSize
+    {
+        get { return _ySize; }
+        set { _ySize = value; }
+    }
+
+}
